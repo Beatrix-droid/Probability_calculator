f54835c3677f5595ef49e8c855bc39a127d12872
Beatrix-droid <amilcare8@gmail.com>
2021-06-18 23:10:00 +0100

    final commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 """ This is a probability calculator. There is a Hat class with an __init__ method that
 akes a number of arguments that specify the number of balls of each colour that are in the
 hat. There is also a draw method that randomly draws a number of balls out of the hat WITHOUT
@@ -20,15 +21,15 @@ class Hat:
             for i in range(value):
                 self.contents.append(key)
 
-
     def draw(self, balls_to_be_drawn):
 
         """ This is a function that randomly draws a specified number of balls
         (called balls_to_be_drawn) out of the hat."""
-        drew_balls = list()
+        drew_balls = []
         self.balls_to_be_drawn = balls_to_be_drawn
+        if balls_to_be_drawn > len(self.contents):
+            return self.contents
         for i in range(balls_to_be_drawn):
-            # drawing a random ball
             ball_drawn = random.choice(self.contents)
             self.contents.remove(ball_drawn)
             drew_balls.append(ball_drawn)
@@ -36,15 +37,22 @@ class Hat:
         return drew_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-#must usee copy hat in here
+
     """ a function that returns the probability of drawing certain colours of balls out of
     the hat over n experiments"""
-
     match = 0
-    balls_drawn_in_experiment_set = set(hat.draw(num_balls_drawn))
-    expected_balls_set = set(expected_balls.keys())
     for i in range(num_experiments):
-        if set(balls_drawn_in_experiment_set).intersection(set(expected_balls_set)) != 0:
-            match = + 1
+        success = True
+        copied_hat = copy.deepcopy(hat)
+        balls_drawn_in_experiment = copied_hat.draw(num_balls_drawn)
+        for colour, count in expected_balls.items():
+            if success:
+                if balls_drawn_in_experiment.count(colour) >= count:
+                    success = True
+                else:
+                    success = False
+            if success:
+                match += 1
     probability = match / num_experiments
+
     return probability
